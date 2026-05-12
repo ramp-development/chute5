a9b8c5dc81783ba31822d889f73433ef049e954c
g4knr <contact@g4knr.co.uk>
2026-05-12 14:56:52 +0100

    Removed log

--- src/components/squares/index.ts
+++ src/components/squares/index.ts
@@ -1,8 +1,6 @@
 import { Square } from './Square';
 
 export const squares = () => {
-  console.log('squares');
-
   const components = document.querySelectorAll<HTMLDivElement>('[data-component="squares"]');
   components.forEach((component) => {
     const instance = new Square(component);
