ccc9d2f46d1921c134c04e227f904b972f7fdd22
g4knr <contact@g4knr.co.uk>
2026-05-21 14:16:36 +0100

    Added smooth scroll

--- src/animation/index.ts
+++ src/animation/index.ts
@@ -1,7 +1,9 @@
 import { glow } from './glow';
+import { smoothScroll } from './smooth-scroll';
 import { workList } from './work-list';
 
 export const animation = () => {
+  smoothScroll();
   workList();
   glow();
 };
